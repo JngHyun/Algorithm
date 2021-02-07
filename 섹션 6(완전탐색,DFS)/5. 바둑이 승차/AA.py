a6e0e4f6d02381a0d98fc405506cdfee87ba1b80
JngHyun <pjh09890989@gmail.com>
2021-02-07 17:50:42 +0900

    0207

--- 섹션 6(완전탐색,DFS)/5. 바둑이 승차/AA.py
+++ 섹션 6(완전탐색,DFS)/5. 바둑이 승차/AA.py
@@ -3,26 +3,29 @@ sys.stdin=open('/Users/pjh/Desktop/박정현/python_algorithm/
 
 def DFS(x,total,tsum):
     global result
-    if total+(all-tsum) < result:
+    if total+(all-tsum) < result: #second : 앞으로의 모든 것을 추가해도 result보다 작은경우 더 볼 필요 없음
         return
-    if total>c:
+    if total>c: # first cut
         return
     if x == n:
-        if total > result:
+        if total > result: #최대값 찾기
             result = total
-
-    else:
+    else: #부분집합 만듬
         DFS(x+1,total+dogs[x],tsum+dogs[x])
         DFS(x+1,total,tsum+dogs[x])
 
 
 if __name__ == '__main__':
     c,n = map(int,input().split())
+
     total = 0
     dogs = []
     result = -247000000
+
     for i in range(n):
         dogs.append(int(input()))
+
     all = sum(dogs)
+
     DFS(0,0,0)
     print(result)
