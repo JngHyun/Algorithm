9dc2da1807e350271df86e7d3a0f7a5a7dc43624
JngHyun <pjh09890989@gmail.com>
2021-02-15 10:30:40 +0900

    0215

--- 섹션 6(완전탐색,DFS)/8. 순열 구하기/AA.py
+++ 섹션 6(완전탐색,DFS)/8. 순열 구하기/AA.py
@@ -1,5 +1,5 @@
 import sys
-sys.stdin = open('/Users/pjh/Desktop/박정현/python_algorithm/섹션 6(완전탐색,DFS)/8. 순열 구하기/in2.txt')
+sys.stdin = open('/Users/pjh/Desktop/박정현/python_algorithm/섹션 6(완전탐색,DFS)/8. 순열 구하기/in3.txt')
 
 # 중복 조합 문제에서 중복인 경우 뻗지 못하게 cut
 
@@ -15,16 +15,16 @@ def DFS(x,res):
     else:
         for i in range(1,n+1):
             if ch[i] == 0: #한번 사용된 경우 출력되지 못하게 중복 cut
-                ch[i] = 1 #사용 여부 check 하기  
+                ch[i] = 1 #사용 여부 check 하기
                 res[x] = i
                 DFS(x+1,res)
-                ch[i]=0 #back하게 되는 경우 사용 여무 check 풀어줌
+                ch[i]=0 #back하게 되는 경우 사용 여무 check 풀어 줌
 
 if __name__ == '__main__':
     n,m = map(int,input().split())
     # n = 3
     # m = 2
-    res = [0] * (m)
+    res = [0] * m
     ch = [0]*(n+1)
     cnt = 0
     DFS(0,res)
