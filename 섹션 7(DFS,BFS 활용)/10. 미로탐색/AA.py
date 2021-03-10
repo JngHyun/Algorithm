0bf38c539be8f895dab6a04d0d87f0bebc7b589e
JngHyun <pjh09890989@gmail.com>
2021-03-10 10:10:57 +0900

    0310

--- 섹션 7(DFS,BFS 활용)/10. 미로탐색/AA.py
+++ 섹션 7(DFS,BFS 활용)/10. 미로탐색/AA.py
@@ -1,29 +1,27 @@
 import sys
 from collections import deque
-sys.stdin = open('/Users/pjh/Desktop/박정현/python_algorithm/섹션 7(DFS,BFS 활용)/10. 미로탐색/in2.txt')
+sys.stdin = open('/Users/pjh/Desktop/박정현/python_algorithm/섹션 7(DFS,BFS 활용)/10. 미로탐색/in5.txt')
 
-a = [list(map(int,input().split())) for _ in range(7)]
+def DFS(x,y):
+    global cnt
+    if x==6 and y==6:
+        cnt +=1
+    else:
+        for i in range(4):
+            xx = x + dx[i]
+            yy = y + dy[i]
+            if 0<=xx<=6 and 0<=yy<=6 and board[xx][yy]!=1:
+                board[xx][yy] = 1
+                DFS(xx,yy)
+                board[xx][yy] = 0 # 백할때 풀어주는거!
 
-dx = [-1,0,1,0]#상하좌우 좌표이동
-dy = [0,1,0,-1]
-ch = [[0]*7 for _ in range(7)]
-Q = deque()
-Q.append((0,0)) 
-a[0][0]=1 #한번 방문한 곳은 벽으로 만듬
+if __name__ =='__main__':
+    board = [list(map(int,input().split())) for _ in range(7)]
+    cnt = 0
+    dx = [-1,0,1,0]
+    dy = [0,1,0,-1]
+    board[0][0] = 1
+    DFS(0,0)
+    print(cnt)
 
-while Q:
-    tmp = Q.popleft()
-    for i in range(4):
-        x = tmp[0] + dx[i]
-        y = tmp[1] + dy[i]
-        if 0<=x<=6 and 0<=y<=6 and a[x][y]==0:
-            a[x][y]=1
-            ch[x][y] = ch[tmp[0]][tmp[1]]+1
-            Q.append((x,y))
-if ch[6][6]==0:
-    print(-1)
-else:
-    print(ch[6][6])
-
-    
     
