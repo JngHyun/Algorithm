108394ff26a6a3fe02fbc3bbe6b4894d1a6ae535
JngHyun <pjh09890989@gmail.com>
2021-03-17 15:01:32 +0900

    0317

--- 섹션 7(DFS,BFS 활용)/14. 안전영역/AA.py
+++ 섹션 7(DFS,BFS 활용)/14. 안전영역/AA.py
@@ -1,36 +1,36 @@
 import sys
-#sys.stdin=open("input.txt", "r")
-dx=[-1, 0, 1, 0]
-dy=[0, 1, 0, -1]
+sys.stdin = open('/Users/pjh/Desktop/박정현/python_algorithm/섹션 7(DFS,BFS 활용)/14. 안전영역/in5.txt')
+dx = [1,0,-1,0]
+dy = [0,1,0,-1]
 sys.setrecursionlimit(10**6)
-def DFS(x, y, h):
-    ch[x][y]=1
-    for i in range(4):
-        xx=x+dx[i]
-        yy=y+dy[i]
-        if 0<=xx<n and 0<=yy<n and ch[xx][yy]==0 and board[xx][yy]>h:
-            DFS(xx, yy, h)
+def DFS(x,y,h):
+    global cnt
+    check[x][y] = 1
+    for k in range(4):
+        xx = x+dx[k]
+        yy = y+dy[k]
+        if 0<=xx<n and 0<=yy<n and board[xx][yy]>=h and check[xx][yy]!= 1:
+            DFS(xx,yy,h)
 
-if __name__=="__main__":
+if __name__=='__main__':
     n = int(input())
-    cnt = 0
+    board=[]
+    height = []
     res = 0
-    board=[list(map(int, input().split())) for _ in range(n)]
-    for h in range(100):
-        ch=[[0]*n for _ in range(n)]
-        cnt=0
+    for _ in range(n):
+        a = list(map(int,input().split()))
+        board.append(a)
+        height+=a
+    for h in set(height): 
+        check = [[0]*n for _ in range(n)]
+        cnt = 0
         for i in range(n):
             for j in range(n):
-                if ch[i][j]==0 and board[i][j]>h:
+                if board[i][j] >= h and check[i][j]==0:
                     cnt+=1
-                    DFS(i, j, h)
-        res=max(res, cnt)
-        if cnt==0:
-            break
+                    DFS(i,j,h)
+        res = max(res,cnt)
     print(res)
-
-
-
 
 
     
