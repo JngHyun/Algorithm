6e9588ef6468def933daaefef39f1da917c78327
JngHyun <pjh09890989@gmail.com>
2021-09-21 15:25:18 +0900

    0921

---
+++ 프로그래머스/고득점키트/완주하지못한선수.py
@@ -0,0 +1,25 @@
+from collections import Counter
+def solution(participant, completion):
+    answer = list((Counter(participant) - Counter(completion)).keys())[0]
+
+def solution(participant, completion):
+    answer = ''
+    temp = 0
+    dic = {}
+    for part in participant:
+        dic[hash(part)] = part
+        temp += int(hash(part))
+    for com in completion:
+        temp -= hash(com)
+    answer = dic[temp]
+
+    return answer
+
+
+def solution(participant, completion):
+    participant.sort()
+    completion.sort()
+    for p, c in zip(participant, completion):
+        if p != c:
+            return p
+    return participant[-1]
