23530eabf7dc2746735f7b6e55088594d998b6d2
JngHyun <pjh09890989@gmail.com>
2021-09-27 01:49:32 +0900

    0927

---
+++ 프로그래머스/위클리챌린지/6주차_복서정렬하기.py
@@ -0,0 +1,19 @@
+def solution(weights, head2head):
+    win_rate = []
+    heavy_count = []
+    nums = []
+    for idx,h in enumerate(head2head):
+        round = [x for x in h if x=='W' or x=='L']
+        if len(round)==0:
+            win_rate.append(0)
+        else:
+            win_rate.append(round.count('W')/(len(round)))
+        heavy_count.append(len([w for i,w in enumerate(weights) if w > weights[idx] and h[i]=='W'] ))
+        nums.append(idx+1)
+
+    res = [(wr,hc,w,n) for wr,hc,w,n in zip(win_rate,heavy_count,weights,nums)]
+    res = sorted(res,key = lambda x:(x[0],x[1],x[2]),reverse=True)
+    res = [r[-1] for r in res]
+
+    return res
+
--- 프로그래머스/위클리챌린지/7주차.py
+++ 프로그래머스/위클리챌린지/7주차.py
@@ -28,3 +28,25 @@ def solution(enter, leave):
         enter.remove(l)    
 
     return answer
+
+
+def solution(enter, leave):
+    answer = [0]*len(enter)
+    room = []
+
+    for e in enter:
+        room.append(e)
+
+        for i in room[:-1]:
+            answer[e-1] += 1
+            answer[i-1] += 1
+
+        while leave:
+            o = leave[0]
+            if o in room:
+                room.remove(o)
+                leave.remove(o)
+            else:
+                break
+
+    return answer
